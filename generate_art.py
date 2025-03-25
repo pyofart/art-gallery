0ae78959b73ec897a72fc2ffe0c633a9a9c7f4b1
pyofart <thjgy@139.com>
2025-03-25 09:15:53 +0800

    Initial commit

---
+++ generate_art.py
@@ -0,0 +1,11 @@
+# generate_art.py
+import matplotlib.pyplot as plt
+import numpy as np
+
+# 示例：生成随机艺术图案
+plt.figure(figsize=(10, 10))
+x = np.linspace(0, 2*np.pi, 100)
+y = np.sin(x**2 + np.random.rand(100))
+plt.plot(x, y)
+plt.savefig("art.png")
+print("Art generated: art.png")
